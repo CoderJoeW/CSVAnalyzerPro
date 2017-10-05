c5dba0936c99a7e96be5fde7c0e13dc625ebc3c5
dl471 <32369415+dl471@users.noreply.github.com>
2017-10-05 02:58:34 +0100

    Track unsaved changes / prompts related to unsaved changes (#11)
    * Add ellipses to certain command buttons
    
    * Add visual indicator for unsaved changes
    
    * Prompt on closing when unsaved files are present
    
    * Prompt on closing when unsaved files are present
    
    * Updating .csproj file

---
+++ CSV Analyzer Pro/CSV Analyzer Pro/TabExtraInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Analyzer_Pro
+{
+    public class TabExtraInfo {
+
+        private int associatedTabIndex; // Will not be updated as tabs are open and closed, be wary of relying on it
+        private string assocaitedFileName;
+        private bool hasUnsavedChanges;
+
+        public TabExtraInfo(int index, string fileName) {
+            associatedTabIndex = index;
+            assocaitedFileName = fileName;
+            hasUnsavedChanges = false;
+        }
+
+        public void SetAssociatedFileName(string filename) {
+            assocaitedFileName = filename;
+        }
+
+        public string GetAssocaitedFileName() {
+            return assocaitedFileName;
+        }
+
+        public int GetAssociatedTabIndex() {
+            return associatedTabIndex;
+        }
+
+        public void SetHasUnsavedChanges(bool yn) {
+            hasUnsavedChanges = yn;
+        }
+
+        public bool QueryHasUnsavedChanges() {
+            return hasUnsavedChanges;
+        }
+
+    }
+}
---
+++ CSV Analyzer Pro/CSV Analyzer Pro/UnsavedChangesMultipleFiles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_Analyzer_Pro {
+    public partial class UnsavedChangesMultipleFiles : Form {
+
+        public int userAnswer;
+        public const int saveAllAndClose = 1;
+        public const int closeWithoutSaving = 2;
+        public const int Cancel = 3;
+
+        public UnsavedChangesMultipleFiles() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            userAnswer = saveAllAndClose;
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            userAnswer = closeWithoutSaving;
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            userAnswer = Cancel;
+            Close();
+        }
+    }
+}
