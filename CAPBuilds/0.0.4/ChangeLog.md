263b06cfd9baed89028c301469733f7a1e5f0394
FlamingGenius <livewiretech.reply.mail@gmail.com>
2017-10-22 12:44:05 -0700

    Removed the context menu

--- CAPBuilds/0.0.4/ChangeLog.md
+++ CAPBuilds/0.0.4/ChangeLog.md
@@ -2,19 +2,22 @@
 
 ### Added
 - Added keyboard shortcuts
+- Context menu on cell right click
+- Commands NewWindow,Open,Save
 
 ### Changed
+- Removed context menu
+- Switch from gridView to spreadsheet view
 
 
 ### Fixed
+- Crash when exiting the application
 
 
 ### TODO
 - Improve plugin system metadata into array structure
 - Add option in installer to auto set csv files to launch progam(options['Open file in new window','Launch progam'])
-- Fix crash related to exiting the application(Error creating window handle)
-- Add edit panel
-- Add context menu on cell right click
 - Create plugins store
 - Create Cloud System
 - Work on config file
+- Update welcome page to look nicer
--- CSV Analyzer Pro/CSV Analyzer Pro/Form1.Designer.cs
+++ CSV Analyzer Pro/CSV Analyzer Pro/Form1.Designer.cs
@@ -41,30 +41,12 @@ namespace CSV_Analyzer_Pro
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addNewRowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.insertAfterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.insertBeforeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.columnsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.insertNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.setToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel3.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -72,145 +54,14 @@ namespace CSV_Analyzer_Pro
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.menuStrip1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(841, 23);
-            this.panel3.TabIndex = 0;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.rowsToolStripMenuItem,
-            this.columnsToolStripMenuItem,
-            this.findToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(841, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newToolStripMenuItem,
-            this.openToolStripMenuItem,
-            this.saveToolStripMenuItem,
-            this.saveAsToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // newToolStripMenuItem
-            // 
-            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
-            this.newToolStripMenuItem.Text = "New";
-            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
-            this.openToolStripMenuItem.Text = "Open...";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
-            this.saveToolStripMenuItem.Text = "Save";
-            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
-            // 
-            // saveAsToolStripMenuItem
-            // 
-            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
-            this.saveAsToolStripMenuItem.Text = "Save As...";
-            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
-            // 
-            // rowsToolStripMenuItem
-            // 
-            this.rowsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.addNewRowToolStripMenuItem,
-            this.insertAfterToolStripMenuItem,
-            this.insertBeforeToolStripMenuItem});
-            this.rowsToolStripMenuItem.Name = "rowsToolStripMenuItem";
-            this.rowsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
-            this.rowsToolStripMenuItem.Text = "Rows";
-            // 
-            // addNewRowToolStripMenuItem
-            // 
-            this.addNewRowToolStripMenuItem.Name = "addNewRowToolStripMenuItem";
-            this.addNewRowToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
-            this.addNewRowToolStripMenuItem.Text = "Insert New";
-            this.addNewRowToolStripMenuItem.Click += new System.EventHandler(this.addNewRowToolStripMenuItem_Click);
-            // 
-            // insertAfterToolStripMenuItem
-            // 
-            this.insertAfterToolStripMenuItem.Name = "insertAfterToolStripMenuItem";
-            this.insertAfterToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
-            this.insertAfterToolStripMenuItem.Text = "Insert After";
-            this.insertAfterToolStripMenuItem.Click += new System.EventHandler(this.insertAfterToolStripMenuItem_Click);
-            // 
-            // insertBeforeToolStripMenuItem
-            // 
-            this.insertBeforeToolStripMenuItem.Name = "insertBeforeToolStripMenuItem";
-            this.insertBeforeToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
-            this.insertBeforeToolStripMenuItem.Text = "Insert Before";
-            this.insertBeforeToolStripMenuItem.Click += new System.EventHandler(this.insertBeforeToolStripMenuItem_Click);
-            // 
-            // columnsToolStripMenuItem
-            // 
-            this.columnsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.insertNewToolStripMenuItem});
-            this.columnsToolStripMenuItem.Name = "columnsToolStripMenuItem";
-            this.columnsToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
-            this.columnsToolStripMenuItem.Text = "Columns";
-            // 
-            // insertNewToolStripMenuItem
-            // 
-            this.insertNewToolStripMenuItem.Name = "insertNewToolStripMenuItem";
-            this.insertNewToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
-            this.insertNewToolStripMenuItem.Text = "Insert New";
-            this.insertNewToolStripMenuItem.Click += new System.EventHandler(this.insertNewToolStripMenuItem_Click);
-            // 
-            // findToolStripMenuItem
-            // 
-            this.findToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.setToolStripMenuItem,
-            this.resetToolStripMenuItem});
-            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
-            this.findToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
-            this.findToolStripMenuItem.Text = "Filter";
-            this.findToolStripMenuItem.Click += new System.EventHandler(this.FindToolStripMenuItem_Click);
-            // 
-            // setToolStripMenuItem
-            // 
-            this.setToolStripMenuItem.Name = "setToolStripMenuItem";
-            this.setToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
-            this.setToolStripMenuItem.Text = "Set...";
-            this.setToolStripMenuItem.Click += new System.EventHandler(this.setToolStripMenuItem_Click);
-            // 
-            // resetToolStripMenuItem
-            // 
-            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
-            this.resetToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
-            this.resetToolStripMenuItem.Text = "Reset";
-            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
-            // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 23);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(841, 290);
+            this.tabControl1.Size = new System.Drawing.Size(841, 313);
             this.tabControl1.TabIndex = 2;
             this.tabControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTabMouseUp);
             // 
@@ -220,7 +71,7 @@ namespace CSV_Analyzer_Pro
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(833, 264);
+            this.tabPage1.Size = new System.Drawing.Size(833, 287);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Welcome";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -233,7 +84,7 @@ namespace CSV_Analyzer_Pro
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(827, 258);
+            this.panel1.Size = new System.Drawing.Size(827, 281);
             this.panel1.TabIndex = 0;
             // 
             // panel2
@@ -242,7 +93,7 @@ namespace CSV_Analyzer_Pro
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 195);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(827, 63);
+            this.panel2.Size = new System.Drawing.Size(827, 86);
             this.panel2.TabIndex = 1;
             // 
             // webBrowser1
@@ -252,7 +103,7 @@ namespace CSV_Analyzer_Pro
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScrollBarsEnabled = false;
-            this.webBrowser1.Size = new System.Drawing.Size(827, 63);
+            this.webBrowser1.Size = new System.Drawing.Size(827, 86);
             this.webBrowser1.TabIndex = 0;
             this.webBrowser1.Url = new System.Uri("http://cof.ftp.sh/CsvAnalyzerPro/welcome.php", System.UriKind.Absolute);
             // 
@@ -271,18 +122,12 @@ namespace CSV_Analyzer_Pro
             // 
             this.ClientSize = new System.Drawing.Size(841, 313);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.panel3);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
@@ -293,29 +138,12 @@ namespace CSV_Analyzer_Pro
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem rowsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem addNewRowToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem insertAfterToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem insertBeforeToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem columnsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem insertNewToolStripMenuItem;
         private TabControl tabControl1;
         private TabPage tabPage1;
-        private ToolStripMenuItem newToolStripMenuItem;
         private Panel panel1;
         private PictureBox pictureBox1;
         private Panel panel2;
         private WebBrowser webBrowser1;
-        private ToolStripMenuItem findToolStripMenuItem;
-        private ToolStripMenuItem setToolStripMenuItem;
-        private ToolStripMenuItem resetToolStripMenuItem;
     }
 }
 
