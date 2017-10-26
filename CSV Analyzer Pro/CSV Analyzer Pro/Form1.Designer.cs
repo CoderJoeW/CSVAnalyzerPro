52a0b6baaa7acd852bfe5508c487636bc47ff8aa
flaminggenius <livewiretech.reply.mail@gmail.com>
2017-10-25 17:12:41 -0700

    Add files via upload

---
+++ CSV Analyzer Pro/CSV Analyzer Pro/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CSV_Analyzer_Pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(841, 313);
+            this.tabControl1.TabIndex = 2;
+            this.tabControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTabMouseUp);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(833, 287);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Welcome";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(827, 281);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.webBrowser1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 195);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(827, 86);
+            this.panel2.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(827, 86);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://cof.ftp.sh/CsvAnalyzerPro/welcome.php", System.UriKind.Absolute);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::CSV_Analyzer_Pro.Properties.Resources.Icon;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(827, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(841, 313);
+            this.Controls.Add(this.tabControl1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private WebBrowser webBrowser1;
+    }
+}
+
---
+++ CSV Analyzer Pro/CSV Analyzer Pro/Form1.cs
@@ -0,0 +1,492 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+using System.Threading;
+using System.Reflection;
+using Syncfusion.Windows.Forms.Grid;
+using Syncfusion.GridHelperClasses;
+using CSV_Analyzer_Pro.Core.PluginSystem;
+using Syncfusion.Windows.Forms.Grid.Grouping;
+using Syncfusion.Windows.Forms.Spreadsheet;
+
+namespace CSV_Analyzer_Pro{
+    public partial class Form1 : Form{
+        #region Globals
+        PluginLoader loader = new PluginLoader();
+
+        DataSet ds = new DataSet();
+
+        BackgroundWorker worker;
+        ShortcutHandler shortcutHandler;
+
+        string path = "";
+
+        bool _exiting = false;
+
+        string Filters = "csv files (*.csv)|*.csv";
+        #endregion
+
+        #region Initializing
+        public Form1(){
+            InitializeComponent();
+
+            //Initiate worker
+            worker = new BackgroundWorker();
+
+            //Worker Event Handlers
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+
+            //Enable Progress Reorting
+            worker.WorkerReportsProgress = true;
+
+            tabControl1.KeyUp += new KeyEventHandler(KeyUpReporter);
+            tabControl1.KeyDown += new KeyEventHandler(KeyDownReporter);
+            tabControl1.KeyDown += new KeyEventHandler(ShortcutChecker);
+            shortcutHandler = new ShortcutHandler();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            try {
+                loader.LoadPlugins();
+            }catch(Exception exc) {
+                Console.WriteLine(string.Format("Plugins couldnt be loaded: {0}", exc.Message));
+            }
+            LoadCommands();
+        }
+
+        private void LoadCommands() {
+            //Register Commands
+        }
+        #endregion
+
+        #region Buttons and Clickers
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            Browse();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                Thread th = new Thread(() => Save(path));
+                th.Start();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                SaveAs(path);
+            }
+        }
+
+        private void addNewRowToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                InsertRowNew();
+            }
+        }
+
+        private void insertAfterToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                InsertRowAfter();
+            }
+        }
+
+        private void insertBeforeToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                InsertRowBefore();
+            }
+        }
+
+        private void insertNewToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                InsertColumnNew();
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) {
+            NewWindow();
+        }
+
+        private void FindToolStripMenuItem_Click(object sender, EventArgs e) {
+            
+        }
+
+        private void setToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                Core.Windows.Filter filter = new Core.Windows.Filter(this.HandleFilter);
+                filter.Show();
+            }
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!IsWelcomePage()) {
+                HandleFilter("Row", "Like", "");
+            }
+        }
+
+        #endregion
+
+        #region Worker Functions
+        private void worker_DoWork(object sender, DoWorkEventArgs e) {
+            OpenCSVFile();
+            worker.ReportProgress(1, DateTime.Now);
+            worker.ReportProgress(20, DateTime.Now);
+            worker.ReportProgress(60, DateTime.Now);
+            worker.ReportProgress(80, DateTime.Now);
+
+            // Lets sleep for a little bit here:  
+            //Thread.Sleep(5000);
+
+            //res 
+            //e.Result = "Done";
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) { 
+            //MessageBox.Show(e.Result.ToString());
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            //Do Nothing
+        }
+        #endregion
+
+        #region EventHandlers
+        private void OnTabMouseUp(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Right) {
+                DialogResult result = MessageBox.Show("Do you really wanna delete this page? All unsaved data will be lost!", "Confirmation", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Yes) {
+                    DeleteTab();
+                }
+            }
+        }
+
+        private void OnColumnHeaderMouseClick(object sender, MouseEventArgs e) {
+            int row, col;
+            GridDataBoundGrid dbg = sender as GridDataBoundGrid;
+            if(dbg.PointToRowCol(new Point(e.X,e.Y),out row,out col)){
+                if(dbg.Model[row,(col - 1)].CellType == "ColumnHeaderCell"){
+                    int tableIndex = tabControl1.SelectedIndex;
+                    EditHeader eh = new EditHeader(this.UpdateHeader);
+                    eh.TextBox1.Text = ds.Tables[tableIndex.ToString()].Columns[(col - 1)].ToString();
+                    eh.TextBox2.Text = (col - 1).ToString();
+                    eh.Show();
+                }
+            }
+        }
+
+        private void OnKeyCommands(object sender,KeyEventArgs e) {
+            //Psuedo code
+        }
+
+        private void KeyDownReporter(object sender, KeyEventArgs e) {
+            shortcutHandler.ReportKeyDown(e.KeyCode.ToString());
+        }
+
+        private void KeyUpReporter(object sender, KeyEventArgs e) {
+            shortcutHandler.ReportKeyUp(e.KeyCode.ToString());
+        }
+
+        private void ShortcutChecker(object sender, KeyEventArgs e) {
+            switch (shortcutHandler.CheckShortcuts()) {
+                case ShortcutHandler.shortcuts.NewWindow:
+                    NewWindow();
+                    ShortcutHandler.Instance.shortcut = ShortcutHandler.shortcuts.NoShortcut;
+                    break;
+                case ShortcutHandler.shortcuts.Open:
+                    Browse();
+                    ShortcutHandler.Instance.shortcut = ShortcutHandler.shortcuts.NoShortcut;
+                    break;
+                case ShortcutHandler.shortcuts.Save:
+                    if (!IsWelcomePage()) {
+                        Thread th = new Thread(() => Save(path));
+                        th.Start();
+                    }
+                    ShortcutHandler.Instance.shortcut = ShortcutHandler.shortcuts.NoShortcut;
+                    break;
+            }
+        }
+
+        private void Model_QueryCellInfo(object sender, Syncfusion.Windows.Forms.Grid.GridQueryCellInfoEventArgs e) {
+            if (e.Style.CellType != "ColumnHeaderCell" && (e.RowIndex % 2 == 0))
+                e.Style.BackColor = Color.LightCyan;
+            else
+                e.Style.BackColor = Color.GhostWhite;
+        }
+        #endregion
+
+        #region Main Functions
+        private void UpdateHeader(string indexVal) {
+            int pageIndex = tabControl1.SelectedIndex;
+            string[] array = indexVal.Split(',');
+            int index = int.Parse(array[0]);
+            //Debug.WriteLine("Index: " + index + " String: " + array[1] + " Table Check: " + ds.Tables[index.ToString()].ToString() + " Column Check: " + ds.Tables[index.ToString()].Columns[index].ColumnName.ToString());
+            ds.Tables[pageIndex.ToString()].Columns[index].ColumnName = array[1];
+        }
+
+        private void HandleFilter(string filterState,string queryState,string queryString) {
+            if(filterState == "Row") {
+                if (queryState == "Like") {
+                    GridDataBoundGrid dbg = tabControl1.SelectedTab.Controls.OfType<GridDataBoundGrid>().First();
+                    DataView dv = ((DataTable)dbg.DataSource).DefaultView;
+                    dv.RowFilter = queryString;
+                    dbg.Refresh();
+                }
+            }
+        }
+        #endregion
+
+        #region DataHandling
+        private void Browse() {
+            if (tabControl1.SelectedIndex == 0) {
+                NewWindow();
+            }
+            OpenFileDialog csvSearch = new OpenFileDialog();
+            csvSearch.Filter = Filters;
+            csvSearch.FilterIndex = 1;
+            csvSearch.Multiselect = false;
+
+            if (csvSearch.ShowDialog() == DialogResult.OK) {
+                int index = tabControl1.SelectedIndex;
+                //Clear Datasource
+                if (ds.Tables.CanRemove(ds.Tables[index.ToString()])) {
+                    ds.Tables.Remove(ds.Tables[index.ToString()]);
+                }
+                DataTable dt = ds.Tables.Add(index.ToString());
+                path = csvSearch.FileName;
+                tabControl1.SelectedTab.Text = csvSearch.FileName;
+                Open(csvSearch.FileName);
+            }
+        }
+
+        private void OpenCSVFile() {
+            CheckForIllegalCrossThreadCalls = false;
+            using (TextFieldParser csvParser = new TextFieldParser(path)) {
+                csvParser.TextFieldType = FieldType.Delimited;
+                csvParser.SetDelimiters(",");
+
+                int index = this.tabControl1.SelectedIndex;
+
+                bool firstLine = true;
+
+                while (!csvParser.EndOfData) {
+                    //proccessing
+                    string[] fields = csvParser.ReadFields();
+
+                    if (firstLine) {
+                        foreach (var val in fields) {
+                            ds.Tables[index.ToString()].Columns.Add(val);
+                        }
+
+                        firstLine = false;
+
+                        continue;
+                    }
+                    //get row data
+                    ds.Tables[index.ToString()].Rows.Add(fields);
+                }
+                //Get gridview
+                GridDataBoundGrid dbg = tabControl1.SelectedTab.Controls.OfType<GridDataBoundGrid>().First();
+                //Attach event handler
+                dbg.MouseDown += (s, e) => this.OnColumnHeaderMouseClick(s, e);
+                //Bind data source
+                dbg.DataSource = ds.Tables[index.ToString()];
+            }
+        }
+        #endregion
+
+        #region Helpers
+        private void DoubleBuffering(GridDataBoundGrid dbg, bool setting) {
+            Type dbgType = dbg.GetType();
+            PropertyInfo pi = dbgType.GetProperty("DoubleBuffered",
+                  BindingFlags.Instance | BindingFlags.NonPublic);
+            pi.SetValue(dbg, setting, null);
+        }
+
+        private void grid_PrepareViewStyleInfo(object sender, GridPrepareViewStyleInfoEventArgs e) {
+
+            if (e.ColIndex == 0 && e.RowIndex > 0) {
+
+                e.Style.Text = e.RowIndex.ToString();
+
+                e.Style.Font.Bold = false;
+
+            }
+
+        }
+
+        public void InitGrid(GridDataBoundGrid dbg) {
+
+            #region DataGridView Contructing
+            dbg.Dock = DockStyle.Fill;
+            dbg.ExcelLikeSelectionFrame = true;
+            dbg.ExcelLikeCurrentCell = true;
+            dbg.Model.Options.SelectionBorderBrush = new SolidBrush(Color.DarkGreen);
+            dbg.Model.Options.SelectionBorderThickness = 4;
+            dbg.ListBoxSelectionMode = SelectionMode.None;
+            dbg.ShowRowHeaders = false;
+            dbg.ThemesEnabled = true;
+            dbg.GridVisualStyles = Syncfusion.Windows.Forms.GridVisualStyles.Metro;
+            DoubleBuffering(dbg, true);
+            dbg.ShowColumnHeaders = true;
+            dbg.AllowResizeToFit = true;
+
+            dbg.BaseStylesMap["Row Header"].StyleInfo.CellType = "Header";
+
+            dbg.Model.QueryCellInfo += new Syncfusion.Windows.Forms.Grid.GridQueryCellInfoEventHandler(Model_QueryCellInfo);
+            #endregion
+        }
+
+        public void InitSpreadsheet(Spreadsheet spreadsheet) {
+            spreadsheet.Dock = DockStyle.Fill;
+
+            spreadsheet.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+        }
+
+        public void InitSpreadsheetRibbon(SpreadsheetRibbon sRibbon) {
+            sRibbon.MenuButtonVisible = false;
+        }
+
+        public bool IsWelcomePage() {
+            if(tabControl1.SelectedIndex == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Exiting Functions
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+            if (!_exiting) {
+                switch (MessageBox.Show(this, "Are you sure you want to exit?", "Closing", MessageBoxButtons.YesNo)) {
+                    case DialogResult.No:
+                        e.Cancel = true;
+                        break;
+                    case DialogResult.Yes:
+                        ExitAll();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void ExitAll() {
+            _exiting = true;
+            try {
+                Application.Exit();
+            }catch(Exception e) {
+                MessageBox.Show("There was an error trying to shutdown.\n\n Try closing all pages and then exiting.");
+            }
+        }
+        #endregion
+
+        #region Commands
+        private void NewWindow() {
+            TabPage tb = new TabPage();
+            Spreadsheet spreadsheet = new Spreadsheet();
+            SpreadsheetRibbon sRibbon = new SpreadsheetRibbon() { Spreadsheet = spreadsheet };
+
+            InitSpreadsheet(spreadsheet);
+            InitSpreadsheetRibbon(sRibbon);
+
+            DataTable dt = new DataTable();
+
+            tb.Text = "New";
+
+            tb.Controls.Add(spreadsheet);
+            tb.Controls.Add(sRibbon);
+            tabControl1.TabPages.Add(tb);
+            tabControl1.SelectedTab = tb;
+        }
+
+        private void NewPluginStoreTab() {
+            TabPage tp = new TabPage();
+
+            WebBrowser wb = new WebBrowser();
+            System.Uri uri = new System.Uri("https://deathcrow.altervista.org/update/PluginStore/index.php");
+            wb.Url = uri;
+
+            tp.Controls.Add(wb);
+            tabControl1.TabPages.Add(tp);
+            tabControl1.SelectedTab = tp;
+        }
+
+        private void Open(string path) {
+            Spreadsheet spreadsheet = tabControl1.SelectedTab.Controls.OfType<Spreadsheet>().First();
+            spreadsheet.Open(path);
+        }
+
+        private void Save(string path) {
+            Spreadsheet spreadsheet = tabControl1.SelectedTab.Controls.OfType<Spreadsheet>().First();
+            spreadsheet.Save();
+        }
+
+        private void SaveAs(string path) {
+            Spreadsheet spreadsheet = tabControl1.SelectedTab.Controls.OfType<Spreadsheet>().First();
+            spreadsheet.SaveAs(path);
+        }
+
+        private void DeleteTab() {
+            Debug.WriteLine("Delete Tab Called");
+            int index = tabControl1.SelectedIndex;
+            tabControl1.TabPages.RemoveAt(index);
+        }
+
+        private void InsertRowNew() {
+            int index = tabControl1.SelectedIndex;
+
+            GridDataBoundGrid dbg = tabControl1.SelectedTab.Controls.OfType<GridDataBoundGrid>().First();
+
+            ds.Tables[index.ToString()].Rows.Add("");
+            dbg.Refresh();
+        }
+
+        private void InsertRowAfter() {
+            int tabCIndex = tabControl1.SelectedIndex;
+
+            GridDataBoundGrid dbg = tabControl1.SelectedTab.Controls.OfType<GridDataBoundGrid>().First();
+            DataRow dr;
+            dr = ds.Tables[tabCIndex.ToString()].NewRow();
+            int index = dbg.CurrentCell.RowIndex;
+            ds.Tables[tabCIndex.ToString()].Rows.InsertAt(dr, index);
+            dbg.Refresh();
+        }
+
+        private void InsertRowBefore() {
+            int tabCIndex = tabControl1.SelectedIndex;
+
+            GridDataBoundGrid dbg = tabControl1.SelectedTab.Controls.OfType<GridDataBoundGrid>().First();
+            DataRow dr;
+            dr = ds.Tables[tabCIndex.ToString()].NewRow();
+            int index = dbg.CurrentCell.RowIndex - 1;
+            ds.Tables[tabCIndex.ToString()].Rows.InsertAt(dr, index);
+        }
+
+        private void InsertColumnNew() {
+            int index = tabControl1.SelectedIndex;
+
+            ds.Tables[index.ToString()].Columns.Add("");
+        }
+        #endregion
+
+        #region Unimplemented Experimental Code
+
+        private void panel5_Paint(object sender, PaintEventArgs e) {
+
+        }
+        #endregion
+    }
+}
